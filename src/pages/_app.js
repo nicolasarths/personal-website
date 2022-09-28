15cd622528d490730333bd3604262315658b9e04
nicolasarths <nicolasarths@hotmail.com>
2022-09-28 18:57:06 -0300

    barrelled styles to clean up imports

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,10 @@
 import Head from 'next/head'
 import Layout from '../components/Layout'
 
-import '../styles/globals.css'
-import '../styles/main.css'
-import '../styles/form.css'
-import '../styles/layout.css'
-import '../styles/under-construction.css'
-import '../styles/projects.css'
-import '../styles/task-dealer.css'
+import '../styles/animations/index.css'
+import '../styles/index.css'
 
 import '../styles/components/index.css'
-import '../styles/animations/index.css'
 
 function MyApp({ Component, pageProps }) {
   return (

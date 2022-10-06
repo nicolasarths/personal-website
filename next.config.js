bd8d78610b1166825a8616853c915a04d6003220
nicolasarths <nicolasarths@hotmail.com>
2022-10-06 16:51:17 -0300

    redirect pt-br forever to avoid 404

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/pt-br',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig

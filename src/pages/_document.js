ef53822a42a865b15bb28d498dd5d09cb3916af8
nicolasarths <nicolasarths@hotmail.com>
2022-10-07 16:39:39 -0300

    add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,21 +1,23 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-        <link 
+          <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400&family=Playfair+Display:ital,wght@0,400;1,400;1,900&display=swap"
-            rel="stylesheet"/>
+            rel="stylesheet"
+          />
+          <link rel="shortcut icon" href="/victor-cortado.jpg" />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;

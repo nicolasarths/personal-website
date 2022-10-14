8962c56e6ddc80d7edb6a602f1dda08bd7828ebf
nicolasarths <nicolasarths@hotmail.com>
2022-10-14 19:09:27 -0300

    refactor buttons behavior and fix missing key props

--- src/components/MenuOfButtons/index.js
+++ src/components/MenuOfButtons/index.js
@@ -1,16 +1,17 @@
 import { Button } from "components/Buttons";
 import styles from "./MenuOfButtons.module.sass"
+import { Fragment } from "react"
 
 const MenuOfButtons = ({ options, className, ...props }) => {
   return (
     <div className={styles.container + (className ? className : "")} {...props}>
       {options.map(({ href, text, snippet }) => (
-        <>
+        <Fragment key={text}>
           {snippet ? <span>{snippet}</span> : ""}
-          <Button key={text} onClick={()=> location.href = href}>
+          <Button onClick={()=> location.href = href}>
             {text}
           </Button>
-        </>
+        </Fragment>
       ))}
     </div>
   );

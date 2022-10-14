1277a0adafd2b9965686b4b149655b36b701215e
nicolasarths <nicolasarths@hotmail.com>
2022-10-14 18:31:00 -0300

    refactor menu of buttons

--- src/components/MenuOfButtons/index.js
+++ src/components/MenuOfButtons/index.js
@@ -1,10 +1,16 @@
-import Option from "../Buttons/Option";
+import { Button } from "components/Buttons";
+import styles from "./MenuOfButtons.module.sass"
 
-const MenuOfButtons = ({options}) => {
+const MenuOfButtons = ({ options, className, ...props }) => {
   return (
-    <div className="full-width">
-      {options.map(({ href, text }) => (
-        <Option key={text} href={href}> {text} </Option>
+    <div className={styles.container + (className ? className : "")} {...props}>
+      {options.map(({ href, text, snippet }) => (
+        <>
+          {snippet ? <span>{snippet}</span> : ""}
+          <Button key={text} onClick={()=> location.href = href}>
+            {text}
+          </Button>
+        </>
       ))}
     </div>
   );

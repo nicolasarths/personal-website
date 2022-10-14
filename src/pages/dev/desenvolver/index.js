fc0536b494d9ca16c56667c9629945dc297b7c54
nicolasarths <nicolasarths@hotmail.com>
2022-10-14 18:44:28 -0300

    abstract page to common/pages folder

--- src/pages/dev/desenvolver/index.js
+++ src/pages/dev/desenvolver/index.js
@@ -1,25 +1,2 @@
-import {
-  DevelopPageContact,
-  DevelopPageComponents,
-  DevelopPageHeader,
-  DevelopPageAttention,
-  DevelopPageOrganize,
-} from "components/DevelopPage";
-import Section from "components/Section";
-
-const DevelopPage = () => {
-  return (
-    <div className="develop-page">
-      <DevelopPageHeader />
-      <Section color="#eda" textColor="var(--background-color)">
-        <p className="lets-explore">Vamos explorar algumas possibilidades?</p>
-      </Section>
-      <DevelopPageComponents />
-      <DevelopPageAttention />
-      <DevelopPageOrganize />
-      <DevelopPageContact />
-    </div>
-  );
-};
-
+import DevelopPage from "common/pages/dev/DevelopPage";
 export default DevelopPage;

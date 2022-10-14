4648c92c40b9897b41428375511ea599a3d4b82a
nicolasarths <nicolasarths@hotmail.com>
2022-10-14 18:39:56 -0300

    started blog mock to test last articles component

---
+++ src/data/blog/index.js
@@ -0,0 +1,23 @@
+import imageSnippet from "assets/homepage/profile-picture.jpg"
+export const articles = [
+  {
+    id: 329,
+    title: "My first one",
+    snippet: "Everything I ever wanted was to be a normal child... But now I grew up!",
+    image: {
+      src: imageSnippet,
+      alt: "example image"
+    },
+    date: "10/10/2022"
+  },
+  {
+    id: 329,
+    title: "My first one",
+    snippet: "Everything I ever wanted was to be a normal child... But now I grew up!",
+    image: {
+      src: imageSnippet,
+      alt: "example image"
+    },
+    date: "10/10/2022"
+  }
+]

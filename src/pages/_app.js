5968bdfe8b2aeafa06b23f7c172fe28ca8c92049
nicolasarths <nicolasarths@hotmail.com>
2022-10-14 18:42:38 -0300

    receive navOptions responsability, add sass styles index, add sass animation for testing purposes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,20 @@
 import Head from "next/head";
 import Layout from "components/Layout";
 import { WindowAlertProvider } from "context/windowAlert";
+import { navOptions } from "data";
 
 import "../styles/animations/index.css";
 import "../styles/index.css";
+import "styles/index.sass"
 
 import "../styles/components/index.css";
+
+import "styles/animations/fadeIn.sass"
 
 function MyApp({ Component, pageProps }) {
   return (
     <WindowAlertProvider>
-      <Layout>
+      <Layout menuOptions={navOptions}>
         <Head>
           <meta name="viewport" content="width=device-width" />
         </Head>

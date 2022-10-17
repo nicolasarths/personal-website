bd3e69058de19317b3f5affb11e9429129e6cc9e
nicolasarths <nicolasarths@hotmail.com>
2022-10-17 20:37:08 -0300

    remove old profile picture

--- src/data/blog/index.js
+++ src/data/blog/index.js
@@ -1,11 +1,9 @@
-import imageSnippet from "assets/homepage/profile-picture.jpg"
 export const articles = [
   {
     id: 329,
     title: "My first one",
     snippet: "Everything I ever wanted was to be a normal child... But now I grew up!",
     image: {
-      src: imageSnippet,
       alt: "example image"
     },
     date: "10/10/2022"
@@ -15,7 +13,6 @@ export const articles = [
     title: "My first one",
     snippet: "Everything I ever wanted was to be a normal child... But now I grew up!",
     image: {
-      src: imageSnippet,
       alt: "example image"
     },
     date: "10/10/2022"

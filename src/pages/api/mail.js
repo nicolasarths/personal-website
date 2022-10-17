0c038fefdda28d4373af88f0a1d82f6809786bdf
nicolasarths <nicolasarths@hotmail.com>
2022-10-17 20:36:32 -0300

    refactor mail api

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -1,24 +1,25 @@
-import mail from "@sendgrid/mail"
+import mail from "@sendgrid/mail";
 
-mail.setApiKey(process.env.SENDGRID_API_KEY)
+mail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async function handler(req, res) {
-  const { subject, text, html } = JSON.parse(req.body)
+  const { subject, text, html } = JSON.parse(req.body);
 
   const data = {
-    to: 'nicolasarths.receiver@gmail.com',
-    from: 'nicolasarths.sender@gmail.com',
+    to: "nicolasarths.receiver@gmail.com",
+    from: "nicolasarths.sender@gmail.com",
     subject,
     text,
-    html
-  }
+    html,
+  };
 
   try {
-    const response = await mail.send(data)
-    if (response.statusCode > 300) res.status(500).json({ status: 'Fail'})
-    else res.status(200).json({ status: 'Ok' })
+    const response = await mail.send(data);
+    if (response.statusCode > 199 && response.statusCode < 300)
+      res.status(200).json({ status: "Ok" });
+    else res.status(500).json({ status: "Fail" });
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ status: 'Fail' })
+    console.log(error);
+    res.status(500).json({ status: "Fail" });
   }
 }

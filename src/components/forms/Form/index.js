bf122e6251d3e99223874bef6660369f6dca7d7f
nicolasarths <nicolasarths@hotmail.com>
2022-10-17 20:37:42 -0300

    refactor form

--- src/components/forms/Form/index.js
+++ src/components/forms/Form/index.js
@@ -1,9 +1,15 @@
-const Form = ({name, method, onSubmit, children}) => {
-    return (
-        <form className="container-small" onSubmit={onSubmit} method={method} id={name} name={name}>
-            {children}
-        </form>
-    )
-}
+const Form = ({ name, method, onSubmit, children }) => {
+  return (
+    <form
+      className="container-small"
+      onSubmit={onSubmit}
+      method={method}
+      id={name}
+      name={name}
+    >
+      {children}
+    </form>
+  );
+};
 
 export default Form;

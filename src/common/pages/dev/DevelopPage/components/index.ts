a7d15fdf5b275983000f81ed1fa9b451a52439b4
nicolasarths <nicolasarths@hotmail.com>
2022-12-06 03:52:34 -0300

    Enhancement (Modify Develop Page & Typescript): Changing what is displayed in the Develop Page. References #17

--- src/common/pages/dev/DevelopPage/components/index.ts
+++ src/common/pages/dev/DevelopPage/components/index.ts
@@ -3,15 +3,6 @@ import Features from "./Features";
 import Header from "./Header";
 import Attention from "./Attention";
 import Organize from "./Organize";
-import Projects from "./Projects";
 import Explore from "./Explore";
 
-export {
-  Contact,
-  Features,
-  Header,
-  Attention,
-  Organize,
-  Projects,
-  Explore
-};
+export { Contact, Features, Header, Attention, Organize, Explore };

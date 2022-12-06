8982978e71ef39f8035884cd7750c2cbbe2bc335
nicolasarths <nicolasarths@hotmail.com>
2022-12-06 03:54:18 -0300

    Enhancement (Modify Develop Page & Typescript): Changing what is displayed in the Develop Page. References #17

---
+++ src/components/Buttons/Button/index.tsx
@@ -0,0 +1,48 @@
+import { PropsWithoutRef, ComponentPropsWithoutRef, ReactNode } from "react";
+import styles from "./Buttons.module.sass";
+
+type acceptedTypes =
+  | "square"
+  | "jumper"
+  | "deep"
+  | "colorChanger"
+  | "fadeIn"
+  | "trembler";
+
+interface ButtonProps {
+  children?: ReactNode;
+  className?: string;
+  type?: acceptedTypes;
+  style?: React.CSSProperties;
+  props?: PropsWithoutRef<any> & ComponentPropsWithoutRef<any>;
+}
+
+const Button = ({ children, className, type, ...props }: ButtonProps) => {
+  const acceptedTypes = [
+    "square",
+    "jumper",
+    "deep",
+    "colorChanger",
+    "fadeIn",
+    "trembler",
+  ];
+  const arrayOfClasses = ["button"];
+
+  if (type) acceptedTypes.includes(type) ? arrayOfClasses.push(type) : 0;
+
+  const stringOfModularClasses = arrayOfClasses
+    .map((cls) => `${styles[cls]} `)
+    .join("");
+
+  const getClasses = className
+    ? className + " " + stringOfModularClasses
+    : stringOfModularClasses;
+
+  return (
+    <button className={getClasses} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;

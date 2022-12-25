d8917bb02fcdfba30de4c183bb207ec4369eaa38
nicolasarths <nicolasarths@hotmail.com>
2022-12-24 21:29:09 -0300

    Enhancement (Blog): Enhancing blog presentation

--- src/common/pages/Homepage/Header/index.tsx
+++ src/common/pages/Homepage/Header/index.tsx
@@ -16,7 +16,7 @@ const Header = () => {
           <a href="#about">Sobre</a>
           <a href="#projects">Projetos</a>
           <Link href="/dev/desenvolver">Desenvolvimento de Websites</Link>
-          <Link href="/blog">Blog</Link>
+          <a href="https://youtube.com/@nicolasarths">Youtube</a>
           <span>
             <a href={hrefs.PERSONAL_WEBSITE}>GH REPO</a>
           </span>

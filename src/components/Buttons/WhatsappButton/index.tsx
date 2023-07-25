9b4d7e59038f550b5bb3f9e91ddf80bbd1daa51f
nicolasarths <nicolasarths@hotmail.com>
2023-07-24 22:29:24 -0300

    Fix (Whatsapp Link): Whatsapp link wasn't working. Fixed number.

--- src/components/Buttons/WhatsappButton/index.tsx
+++ src/components/Buttons/WhatsappButton/index.tsx
@@ -2,6 +2,7 @@ import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "components/Buttons";
 import { ReactNode } from "react";
+import { hrefs } from "data";
 
 import styles from "./WhatsappButton.module.sass";
 
@@ -10,11 +11,7 @@ type Props = {
 };
 
 const WhatsappButton = ({ children }: Props) => (
-  <a
-    href="https://wa.me/051984376635"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
+  <a href={hrefs.WHATSAPP_BASE} target="_blank" rel="noopener noreferrer">
     <Button type="square" className={styles.whatsappButton}>
       <FontAwesomeIcon icon={faWhatsapp} style={{ height: "26px" }} />
       {children}
--- src/data/hrefs/index.js
+++ src/data/hrefs/index.js
@@ -11,6 +11,7 @@ const hrefs = {
   ...locals,
   ...profiles,
   ...repositories,
+  WHATSAPP_BASE: "https://wa.me/51984376635",
 };
 
 export default hrefs;
